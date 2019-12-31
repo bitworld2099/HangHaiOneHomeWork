849201fb28110673cd2c82e13a0be6a1b3d9a436
ivey <ivey@126.com>
2019-12-31 19:51:07 +0800

    项目初始提交

---
+++ springdata-demo/src/main/java/com/lhj/config/MyMvcConfig.java
@@ -0,0 +1,19 @@
+package com.lhj.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * mvc视图配置器
+ * @author Shinelon
+ */
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("crud");
+        registry.addViewController("/main").setViewName("crud");
+    }
+
+}
