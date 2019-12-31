75496313cf0afecc97fce6acfd19dfe8e4313e84
liao <541381458@qq.com>
2019-12-31 14:48:09 +0800

    计科16239廖健的实训作业

---
+++ springboot-team4-liaojian/src/main/java/cn/crud/pojo/ResponseObject.java
@@ -0,0 +1,17 @@
+package cn.crud.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ResponseObject {
+    private String msg;
+    private String code;
+    private Object object;
+
+}
