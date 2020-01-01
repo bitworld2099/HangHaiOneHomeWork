7f52e750ccbee682549dfd4d7b02f0ab5e28f2e8
FanYin-1996 <1144290250@qq.com>
2020-01-01 23:40:22 +0800

    计科163-01-孔天泽

---
+++ spring_boot_crud_team_KongTianZe/src/main/java/com/tianze/ApplicatiionStarter.java
@@ -0,0 +1,11 @@
+package com.tianze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicatiionStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicatiionStarter.class,args);
+    }
+}
