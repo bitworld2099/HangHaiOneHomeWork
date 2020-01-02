cc805473c9cb7850aa0ad0953f67b0aeeeeda472
anzzz999 <995127051@qq.com>
2020-01-02 09:08:02 +0800

    计科164甘广华

---
+++ springboot-curd-team13-ganguanghua/src/test/java/com/ggh/SpringdataDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ggh;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
